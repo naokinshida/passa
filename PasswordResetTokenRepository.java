e5ee306a9d513c4644053a75f42d3cd05f594f59
naokinshida <naodance246@gmail.com>
2024-09-24 13:49:15 +0900

    first commit

---
+++ PasswordResetTokenRepository.java
@@ -0,0 +1,10 @@
+package com.example.nagoyameshi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.nagoyameshi.entity.PasswordResetToken;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    PasswordResetToken findByToken(String token);
+    void deleteByUserId(Integer id);
+}
